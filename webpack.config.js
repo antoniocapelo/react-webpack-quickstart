d2b1e89d340cceaacd0749ad7d305cc370292d5e
Capelo <antonio.c.capelo@gmail.com>
2015-12-10 23:30:49 +0000

    adding historyApiFallback on dev server

--- webpack.config.js
+++ webpack.config.js
@@ -5,16 +5,19 @@
 // webpack -d to include source maps
 
 module.exports = {
-  entry: './app/index.js',
-  output: {
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel-loader' },
-      { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' }, // use ! to chain loaders
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
-      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' } // inline base64 URLs for <=8k images, direct URLs for the rest
-    ]
-  }
+    entry: './app/index.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, loader: 'babel-loader' },
+            { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' }, // use ! to chain loaders
+            { test: /\.css$/, loader: 'style-loader!css-loader' },
+            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' } // inline base64 URLs for <=8k images, direct URLs for the rest
+        ]
+    }
 };
